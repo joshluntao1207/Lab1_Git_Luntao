2f56c19b31f4ee2bcc29c94da6bef1c7ad3e41f6
Josh Vincent H. Luntao <joshvincent.luntao@tup.edu.ph>
2026-09-12 10:47:14 +0800

    Initial commit by Student Luntao

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 print("Lab 1: Version Control")
-print("Author: Josh Vincent H. Luntao")
-print("Student ID: TUPM-26-1947")
-print("Class Section: [BSECE-1D]")
-print("Login Feature Enabled")
+print("Author: [Josh Vincent H. Luntao]")   
+print("Student ID: TUPM-26-1947")  
