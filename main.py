2175e6c98bdc53c13759bad7cdf2e6df4c7a4ce2
Josh Vincent H. Luntao <joshvincent.luntao@tup.edu.ph>
2026-09-12 09:54:55 +0800

    Added section info for Luntao

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Lab 1: Version Control")
-print("Author: [Josh Vincent H. Luntao]")
+print("Lab 1: Version Control")
+print("Author: Josh Vincent H. Luntao")
 print("Student ID: TUPM-26-1947")
 print("Class Section: [BSECE-1D]")
